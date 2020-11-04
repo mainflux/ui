393be304c49fc93cc058bed5c7365888859040c3
Manuel Imperiale <manuel.imperiale@gmail.com>
2020-11-04 23:51:44 +0100

    NOISSUE - Rename Dashboard -> Home
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,9 +2,9 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'Dashboard',
+    title: 'Home',
     icon: 'home-outline',
-    link: '/pages/dashboard',
+    link: '/pages/home',
     home: true,
   },
   {
