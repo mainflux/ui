3582728e35c1ad24fb6b30318f5edb96a3212101
Darko Draskovic <darko.draskovic@gmail.com>
2020-12-03 16:06:02 +0100

    Add default environment
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- src/environments/environment.experimental.ts
+++ src/environments/environment.experimental.ts
@@ -1,37 +1,8 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+import { environment as defaultEnvironment } from './environment.defaults';
 
 export const environment = {
   experimental: true,
   production: false,
   
-  usersUrl: '/users',
-  groupsUrl: '/groups',
-  usersVersionUrl: '/version',
-  loginUrl: '/tokens',
-  requestPassUrl: '/password/reset-request',
-  resetPassUrl: '/password/reset',
-  changePassUrl: '/password',
-  thingsUrl: '/things',
-  twinsUrl: '/twins',
-  statesUrl: '/states',
-  channelsUrl: '/channels',
-  bootstrapConfigsUrl: '/bootstrap/things/configs',
-  bootstrapUrl: '/bootstrap/things/bootstrap',
-  connectUrl: '/connect',
-  browseUrl: '/browse',
-
-  writerChannelsUrl: '/writer/channels',
-  readerChannelsUrl: '/reader/channels',
-  messagesSufix: 'messages',
-
-  mqttWsUrl: window['env']['mqttWsUrl'] || 'ws://localhost/mqtt',
-  exportConfigFile: '/configs/export/config.toml',
+    ...defaultEnvironment,
 };
