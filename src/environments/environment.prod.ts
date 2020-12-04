31a83074b020a2d5cabcc6c1769167a09bae00aa
Ivan Milosevic <iva@blokovi.com>
2020-12-04 19:12:50 +0100

    rebase
    Signed-off-by: Ivan Milosevic <iva@blokovi.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,27 +3,6 @@ import { environment as defaultEnvironment } from './environment.defaults';
 export const environment = {
   experimental: false,
   production: true,
-  
-  ...defaultEnvironment,
-  usersUrl: '/users',
-  groupsUrl: '/groups',
-  usersVersionUrl: '/version',
-  loginUrl: '/tokens',
-  requestPassUrl: '/password/reset-request',
-  resetPassUrl: '/password/reset',
-  changePassUrl: '/password',
-  thingsUrl: '/things',
-  twinsUrl: '/twins',
-  statesUrl: '/states',
-  channelsUrl: '/channels',
-  bootstrapConfigsUrl: '/bootstrap/things/configs',
-  bootstrapUrl: '/bootstrap/things/bootstrap',
-  connectUrl: '/connect',
-  browseUrl: '/browse',
 
-  writerChannelsUrl: '/writer/ch',
-  readerChannelsUrl: '/reader/ch',
-
-  mqttWsUrl: window['env']['mqttWsUrl'] || 'ws://localhost/mqtt',
-  exportConfigFile: '/configs/export/config.toml',
+    ...defaultEnvironment,
 };
