01e121f75c69fc46801d37838510f205b4c5bb0b
Darko Draskovic <darko.draskovic@gmail.com>
2021-03-16 17:28:08 +0100

    NOISSUE - Remove experimental build and use only dev and prod build (#183)
    * Remove experimental environment
    
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>
    
    * Remove experimental environment property
    
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>
    
    * Add conditional build to routing
    
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 import { environment as defaultEnvironment } from './environment.defaults';
 
 export const environment = {
-  experimental: false,
   production: false,
 
     ...defaultEnvironment,
