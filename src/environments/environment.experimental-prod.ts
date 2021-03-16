df628a4c2fbfbfaa05556627a014d979230b6488
Darko Draskovic <darko.draskovic@gmail.com>
2021-03-16 11:41:39 +0100

    Remove experimental environment
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- src/environments/environment.experimental-prod.ts
+++
@@ -1,8 +0,0 @@
-import { environment as defaultEnvironment } from './environment.defaults';
-
-export const environment = {
-  experimental: true,
-  production: true,
-
-    ...defaultEnvironment,
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@ import { environment as defaultEnvironment } from './environment.defaults';
 
 export const environment = {
   experimental: false,
-  production: false,
 
     ...defaultEnvironment,
 };
