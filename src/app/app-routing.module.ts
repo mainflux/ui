4cafc04d94a4c698d0dabb0c0fd0d9ddfb96f783
mteodor <mirko.teodorovic@gmail.com>
2021-11-25 15:13:34 +0100

    align path routing with parent route
    Signed-off-by: mteodor <mirko.teodorovic@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -58,6 +58,7 @@ export const routes: Routes = [
           },
         ],
       },
+      { path: '', redirectTo: 'pages', pathMatch: 'full' },
       { path: '**', redirectTo: 'pages' },
     ],
   },
