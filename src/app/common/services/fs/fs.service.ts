a63a8e4d65d0568389f20bc2cc20c0a02397aff0
Manuel Imperiale <manuel.imperiale@gmail.com>
2021-11-25 20:05:45 +0100

    Use prettyJson for saving
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- src/app/common/services/fs/fs.service.ts
+++ src/app/common/services/fs/fs.service.ts
@@ -15,26 +15,15 @@ export class FsService {
       return;
     }
 
-    let jsonContent = '[';
-    rows.forEach((row, i) => {
-      let rowJson = '';
-      try {
-        rowJson = JSON.stringify(row);
-      } catch (e) {
-        this.notificationsService.warn('Failed to convert to JSON', '');
-      }
-
-      if (rows.length > 1 && i !== rows.length - 1) {
-        jsonContent = jsonContent + rowJson + ',' + '\n';
-      } else {
-        jsonContent = jsonContent + rowJson;
-      }
-    });
-
-    jsonContent = jsonContent + ']';
+    let jsonRows = '';
+    try {
+      jsonRows = JSON.stringify(rows, null, 2);
+    } catch (e) {
+      this.notificationsService.warn('Failed to convert to JSON', '');
+    }
 
     const bom = new Uint8Array([0xEF, 0xBB, 0xBF]); // UTF-8 BOM
-    const blob = new Blob([bom, jsonContent], { type: 'text/json;charset=utf-8;' });
+    const blob = new Blob([bom, jsonRows], { type: 'text/json;charset=utf-8;' });
     if (navigator.msSaveBlob) { // IE 10+
       navigator.msSaveBlob(blob, filename);
     } else {
