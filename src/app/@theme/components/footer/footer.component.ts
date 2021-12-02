d9824ea922a3bc3f73c90a73b5cc65d3c69adeab
iSamBa <bahou.aissam@gmail.com>
2021-12-02 17:14:08 +0100

    Fix lint errors
    Signed-off-by: iSamBa <bahou.aissam@gmail.com>

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
-import { STRINGS } from "assets/text/strings";
+import { Component } from '@angular/core';
+import { STRINGS } from 'assets/text/strings';
 
 @Component({
-  selector: "ngx-footer",
-  templateUrl: "./footer.component.html",
-  styleUrls: ["./footer.component.scss"],
+  selector: 'ngx-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   disclaimer: string = STRINGS.footer.disclaimer;
