a94eecf3c4ad359b92fe86162b5dd2fb997e8133
Manuel Imperiale <manuel.imperiale@gmail.com>
2022-03-08 16:49:27 +0100

    NOISSUE - Fix env.template.js
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -1,9 +1,9 @@
 (function(window) {
-    window["env"] = window["env"] || {};
-  
-    // Environment variables
-    window["env"]["mqttWsUrl"] = "${MF_UI_MQTT_WS_URL}";
-    window["env"]["proxyAuth"] = ${MF_PROXY_AUTH};
-    window["env"]["logoutUrl"] = "${MF_PROXY_LOGOUT_URL}";
-    window["env"]["appPrefix"] = "${MF_UI_APP_PREFIX}";
-  })(this);
+  window["env"] = window["env"] || {};
+  var envProxyAuth = "${MF_PROXY_AUTH}";
+  // Environment variables
+  window["env"]["mqttWsUrl"] = "${MF_UI_MQTT_WS_URL}";
+  window["env"]["proxyAuth"] = envProxyAuth === 'true';
+  window["env"]["logoutUrl"] = "${MF_PROXY_LOGOUT_URL}";
+  window["env"]["appPrefix"] = "${MF_UI_APP_PREFIX}";
+})(this);
