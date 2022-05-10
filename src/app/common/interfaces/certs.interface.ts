b77d9e758e08a808d0af9b4aec671a8554a44390
Manuel Imperiale <manuel.Imperiale@gmail.com>
2022-05-10 21:01:05 +0200

    NOISSUE - Retrieve client key on cert issuing (#255)
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- src/app/common/interfaces/certs.interface.ts
+++ src/app/common/interfaces/certs.interface.ts
@@ -1,6 +1,14 @@
-export interface Cert {
+export interface CertReq {
   thing_id?: string;
   key_bits?: number;
   key_type?: string;
   ttl?: string;
+}
+
+export interface CertRes {
+  thing_id?: string;
+  client_cert?: string;
+  client_key?: string;
+  cert_serial?: string;
+  expiration?: string;
 }
